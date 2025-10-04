2aa97ba708e35a9508bfe6f8848b38550e164efd
kimjeongho930829 <kimjeongho930829@gmail.com>
2025-10-04 19:18:47 +0900

    clerk 인증 설정

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -18,6 +18,8 @@ import poppins700 from "@fontsource/poppins/700.css?url"
 import poppins800 from "@fontsource/poppins/800.css?url"
 import poppins900 from "@fontsource/poppins/900.css?url"
 import { ChartColumnBigIcon } from 'lucide-react'
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/tanstack-react-start'
+import { Button } from '@/components/ui/button'
 
 export const Route = createRootRoute({
   head: () => ({
@@ -59,19 +61,43 @@ function RootComponent() {
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <html>
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        <nav className='bg-primary p-4 h-20 text-white flex items-center justify-between'>
-          <Link to='/' className='flex gap-1 items-center font-blod text-2xl'>
-            <ChartColumnBigIcon className='text-lime-500' /> TanTracker
-          </Link>
-        </nav>
-        {children}
-        <Scripts />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html>
+        <head>
+          <HeadContent />
+        </head>
+        <body>
+          <nav className='bg-primary p-4 h-20 text-white flex items-center justify-between'>
+            <Link to='/' className='flex gap-1 items-center font-blod text-2xl'>
+              <ChartColumnBigIcon className='text-lime-500' /> TanTracker
+            </Link>
+            <div>
+              <SignedOut>
+                <div className="text-white flex items-center">
+                  <Button asChild variant="link" className="text-white">
+                    <SignInButton />
+                  </Button>
+                  <div className="w-[1px] h-8 bg-zinc-700" />
+                  <Button asChild variant="link" className="text-white">
+                    <SignUpButton />
+                  </Button>
+                </div>
+              </SignedOut>
+              <SignedIn>
+                <UserButton showName appearance={{
+                  elements: {
+                    userButtonOuterIdentifier: {
+                      color: "white"
+                    }
+                  }
+                }}/>
+              </SignedIn>
+            </div>
+          </nav>
+          {children}
+          <Scripts />
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
